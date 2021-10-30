5dddd1818be1b32a7b6eec9d5ff4d1a219eed5d5
paperflight <hufh@live.cn>
2021-10-30 14:39:20 +0100

    Add files via upload

--- autofill.py
+++ autofill.py
@@ -13,7 +13,7 @@ SUBTYPE_KEY = '/Subtype'
 WIDGET_SUBTYPE_KEY = '/Widget'
 
 data_dict={
-    'page0_Line4_DaytimeTelephoneNumber[0]':'Mouren',
+    'form[0].#subform[0].Line4_DaytimeTelephoneNumber[0]':'Mouren',
     'page0_Pt1Line2c_MiddleName[0]':'Mail'
 }
 #data_dict={
@@ -28,17 +28,66 @@ def inspect_value(input_pdf_path):
         for annotation in annotations:
             if annotation[SUBTYPE_KEY]==WIDGET_SUBTYPE_KEY:
                 key=annotation[ANNOT_FIELD_KEY][1:-1]
-                print(key, '"' + str(annotation['/V']) + '"', '"' + str(annotation['/AS']) + '"')
+                try:
+                    key = str(bytes.fromhex(key).decode('utf-16'))
+                except ValueError:
+                    key = key.split('.')[-1]
+                key = 'page' + str(page_number) + '_' +key
+                if annotation[ANNOT_FORM_type] == ANNOT_FORM_button:
+                    checkbox_dict = annotation['/AP']['/D']
+                    on_key = ''
+                    for on_keys in checkbox_dict.keys():
+                        if on_keys != '/Off':
+                            on_key = on_keys[1::]
+                            break
+                    print(key, '"' + str(annotation['/V']) + '"', '"' + str(on_key) + '"')
+                else:
+                    print(key, '"' + str(annotation['/V']) + '"')
                 key_list.append(key)
-        
-def inspect(input_pdf_path, input_excel_path=None):
+                
+def inspect_print(input_pdf_path):
     key_list = []
     template_pdf=pdfrw.PdfReader(input_pdf_path)
+    template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
     for page_number, page in enumerate(template_pdf.pages):
         annotations=page[ANNOT_KEY]
         for annotation in annotations:
             if annotation[SUBTYPE_KEY]==WIDGET_SUBTYPE_KEY:
                 key=annotation[ANNOT_FIELD_KEY][1:-1]
+                try:
+                    key = str(bytes.fromhex(key).decode('utf-16'))
+                except ValueError:
+                    key = key.split('.')[-1]
+                if annotation[ANNOT_FORM_type] == ANNOT_FORM_button:
+                    checkbox_dict = annotation['/AP']['/D']
+                    on_key = ''
+                    for on_keys in checkbox_dict.keys():
+                        if on_keys != '/Off':
+                            on_key = on_keys[1::]
+                            break
+                    annotation.update(
+                        pdfrw.PdfDict(V=pdfrw.PdfName(on_key), AS=pdfrw.PdfName(on_key)) # default checkbox value is 'Off'
+                    )
+                else:
+                    annotation.update(
+                        pdfrw.PdfDict(V=pdfrw.PdfName(key)) # default checkbox value is 'Off'
+                    )
+    pdfrw.PdfWriter().write(input_pdf_path[0:-4] + '-investigate.pdf',template_pdf)
+        
+def inspect(input_pdf_path, input_excel_path=None):
+    key_list = []
+    template_pdf=pdfrw.PdfReader(input_pdf_path)
+    template_pdf.Root.AcroForm.update(pdfrw.PdfDict(NeedAppearances=pdfrw.PdfObject('true')))
+    for page_number, page in enumerate(template_pdf.pages):
+        annotations=page[ANNOT_KEY]
+        for annotation in annotations:
+            if annotation[SUBTYPE_KEY]==WIDGET_SUBTYPE_KEY:
+                key=annotation[ANNOT_FIELD_KEY][1:-1]
+                try:
+                    key = str(bytes.fromhex(key).decode('utf-16'))
+                except ValueError:
+                    key = key.split('.')[-1]
+                key = 'page' + str(page_number) + '_' +key
                 print(key)
                 key_list.append(key)
     if input_excel_path is None:
@@ -71,10 +120,27 @@ def write_fillable_pdf(input_pdf_path,output_pdf_path,data_dict):
             if annotation[SUBTYPE_KEY]==WIDGET_SUBTYPE_KEY:
                 key=annotation[ANNOT_FIELD_KEY][1:-1]
                 try:
+                    key = str(bytes.fromhex(key).decode('utf-16'))
+                except ValueError:
+                    key = key.split('.')[-1]
+                key = 'page' + str(page_number) + '_' +key
+                try:
                     if annotation[ANNOT_FORM_type] == ANNOT_FORM_button:
-                        annotation.update(
-                            pdfrw.PdfDict(V=pdfrw.PdfName(data_dict[key]), AS=pdfrw.PdfName(data_dict[key])) # default checkbox value is 'Off'
-                        )
+                        checkbox_dict = annotation['/AP']['/D']
+                        on_key = ''
+                        for on_keys in checkbox_dict.keys():
+                            if on_keys != '/Off':
+                                on_key = on_keys[1::]
+                                print(on_key)
+                                break
+                        if 'Yes' in data_dict[key] or 'yes' in data_dict[key] or 'On' in data_dict[key] or on_key == data_dict[key]:
+                            annotation.update(
+                                pdfrw.PdfDict(V=pdfrw.PdfName(on_key), AS=pdfrw.PdfName(on_key)) # default checkbox value is 'Off'
+                            )
+                        else:
+                             annotation.update(
+                                pdfrw.PdfDict(V=pdfrw.PdfName('Off'), AS=pdfrw.PdfName('Off')) # default checkbox value is 'Off'
+                            )
                     else:
                         annotation.update(
                             pdfrw.PdfDict(V='{}'.format(data_dict[key]))
@@ -109,12 +175,16 @@ def run_all(input_excel_path):
         sheet = workbook.worksheets[sheet_name_index]
         print('Extracting from ' + sheet.title)
         for data in sheet.iter_rows(min_col=1, max_col=2, values_only=True):
+            key = data[0]
+            if data[0] is not None and len(data[0].split('.')) > 1:
+                page_number = data[0].split('.')[-2]
+                key = 'page' + str(page_number[-2]) + '_' + data[0].split('.')[-1]
             if data[1] is None:
-                print(str(data[0]), '')
-                data_dict[data[0]] = ''
+                print(str(key), '')
+                data_dict[str(key)] = ''
             else:
-                print(data[0], data[1])
-                data_dict[str(data[0])] = str(data[1])
+                print(key, data[1])
+                data_dict[str(key)] = str(data[1])
         write_fillable_pdf(path + sheet_name + '.pdf', path + sheet_name + '-fill.pdf', data_dict)
     
     
@@ -126,6 +196,8 @@ if __name__ == '__main__':
             inspect(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'inspect_value':
         inspect_value(sys.argv[2])
+    elif sys.argv[1] == 'inspect_print':
+        inspect_print(sys.argv[2])
     elif sys.argv[1] == 'decrpt':
         decrpt(sys.argv[2])
     elif sys.argv[1] == 'write':
